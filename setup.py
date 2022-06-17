7820b78f3bd61f92dbfd2033d41ac4d7e871a70c
d-01 <deman-13@mail.ru>
2022-06-17 09:22:58 +0300

    Fix: installation script

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name='instacartlib',
     version='0.1',
-    packages=['instacartlib'],
+    # * Automatic package discoverty:
+    #   find_packages(include=['instacartlib', 'instacartlib.*'])
+    packages=['instacartlib', 'instacartlib.feature_extractors'],
     long_description="Library to work with instacart dataset.",
 )
